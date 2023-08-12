f9e5b3e3379f2663dff162488c71c45bfbb059c3
phen0m1nal <nmnashrf@gmail.com>
2023-08-12 14:51:29 +0500

    2.1

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -1,17 +1,17 @@
 /*=============== FILTERS TABS ===============*/
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+    tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab =>{
-    tab.addEventListener('click', () =>{
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target)
 
-        tabContents.forEach(tc =>{
+        tabContents.forEach((tc) => {
             tc.classList.remove('filters__active')
         })
         target.classList.add('filters__active')
 
-        tabs.forEach(t =>{
+        tabs.forEach((t) => {
             t.classList.remove('filter-tab-active')
         })
         tab.classList.add('filter-tab-active')
@@ -28,14 +28,30 @@ const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+const getCurrentTheme = () =>
+    document.body.classList.contains(darkTheme) ? 'light' : 'dark'
+const getCurrentIcon = () =>
+    themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+        darkTheme
+    )
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](
+        iconTheme
+    )
+}
+
+document.onload = () => {
+    if (getCurrentTheme() === 'dark') {
+        document.getElementById('fiverr').style.fill = 'black'
+        document.getElementById('upwork').style.fill = 'black'
+    } else {
+        document.getElementById('fiverr').style.fill = 'white'
+        document.getElementById('upwork').style.fill = 'white'
+    }
 }
 
 // Activate / deactivate the theme manually with the button
@@ -46,6 +62,13 @@ themeButton.addEventListener('click', () => {
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
+    if (getCurrentTheme() === 'dark') {
+        document.getElementById('fiverr').style.fill = 'black'
+        document.getElementById('upwork').style.fill = 'black'
+    } else {
+        document.getElementById('fiverr').style.fill = 'white'
+        document.getElementById('upwork').style.fill = 'white'
+    }
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
@@ -57,10 +80,10 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.profile__border`)
-sr.reveal(`.profile__name`, {delay: 500})
-sr.reveal(`.profile__profession`, {delay: 600})
-sr.reveal(`.profile__social`, {delay: 700})
-sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
-sr.reveal(`.profile__buttons`, {delay: 800})
-sr.reveal(`.filters__content`, {delay: 900})
-sr.reveal(`.filters`, {delay: 1000})
+sr.reveal(`.profile__name`, { delay: 500 })
+sr.reveal(`.profile__profession`, { delay: 600 })
+sr.reveal(`.profile__social`, { delay: 700 })
+sr.reveal(`.profile__info-group`, { interval: 100, delay: 700 })
+sr.reveal(`.profile__buttons`, { delay: 800 })
+sr.reveal(`.filters__content`, { delay: 900 })
+sr.reveal(`.filters`, { delay: 1000 })
